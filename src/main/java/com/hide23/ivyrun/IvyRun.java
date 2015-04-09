4a43c7947a954621211338c0fccf3fd3a67273d8
Hideaki Takahashi <mymelo@gmail.com>
2015-04-08 23:50:15 -0400

    use main method of Ivy CLI to resolve dependencies

--- src/main/java/com/hide23/ivyrun/IvyRun.java
+++ src/main/java/com/hide23/ivyrun/IvyRun.java
@@ -1,107 +1,78 @@
 package com.hide23.ivyrun;
 
-import org.apache.ivy.Ivy;
-import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
-import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
-import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
-import org.apache.ivy.core.module.id.ModuleRevisionId;
-import org.apache.ivy.core.report.ResolveReport;
-import org.apache.ivy.core.resolve.ResolveOptions;
-import org.apache.ivy.core.retrieve.RetrieveOptions;
-import org.apache.ivy.core.settings.IvySettings;
-import org.apache.ivy.plugins.resolver.BintrayResolver;
-import org.apache.ivy.plugins.resolver.ChainResolver;
-import org.apache.ivy.plugins.resolver.DependencyResolver;
-
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
+import java.io.Writer;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Execute an executable jar from artifactory
  */
 public class IvyRun {
-    private final Ivy ivy;
+    Path setting;
 
     /**
      * @param repos repositories. will be used as resolvers in a chain resolver.
      *              if it is empty, default BintrayResolver will be used
      */
     public IvyRun(List<URI> repos) {
-        IvySettings setting = new IvySettings();
-        ivy = Ivy.newInstance(setting);
-        ChainResolver resolver = new ChainResolver();
-        resolver.setReturnFirst(true);
-        resolver.setName("chained");
-        convert(repos).forEach(x -> resolver.add(x));
-        setting.addResolver(resolver);
-        setting.setDefaultResolver(resolver.getName());
+        try {
+            setting = Files.createTempFile("", "");
+            try (Writer w = Files.newBufferedWriter(setting, StandardCharsets.UTF_8)) {
+                XMLStreamWriter x = XMLOutputFactory.newInstance().createXMLStreamWriter(w);
+                x.writeStartDocument();
+                x.writeStartElement("ivysettings");
+                x.writeStartElement("settings");
+                x.writeAttribute("defaultResolver", "chain");
+                x.writeEndElement();
+                x.writeStartElement("caches");
+                x.writeEndElement();
+                x.writeStartElement("resolvers");
+                x.writeStartElement("chain");
+                x.writeAttribute("name", "chain");
+                if (repos.isEmpty()) {
+                    x.writeStartElement("bintray");
+                    x.writeEndElement();
+                } else {
+                    for (URI u : repos) {
+                        x.writeStartElement("ibiblio");
+                        x.writeAttribute("m2compatible", "true");
+                        x.writeAttribute("root", u.toString());
+                        x.writeEndElement();
+                    }
+                }
+                x.writeEndElement();
+                x.writeEndElement();
+                x.writeEndDocument();
+            }
+        } catch (IOException | XMLStreamException e) {
+            e.printStackTrace();
+        }
     }
 
     public IvyRun(URI... repos) {
         this(Arrays.asList(repos));
     }
 
-    private static BintrayResolver c() {
-        BintrayResolver resolver = new BintrayResolver();
-        resolver.setM2compatible(true);
-        return resolver;
-    }
-
-    private static DependencyResolver c(URI u) {
-        BintrayResolver resolver = c();
-        resolver.setName(u.toString());
-        resolver.setUsepoms(true);
-        resolver.setRoot(u.toString());
-        return resolver;
-    }
-
-    private Stream<DependencyResolver> convert(List<URI> repos) {
-        if (repos.isEmpty()) {
-            return Stream.of(c());
-        }
-        return repos.stream().map(IvyRun::c);
-    }
-
-    private ModuleDescriptor makeDescriptor(String org, String name, String rev) {
-        DefaultModuleDescriptor desc = DefaultModuleDescriptor.newDefaultInstance(
-                ModuleRevisionId.newInstance("fake-organization", name, rev)
-        );
-
-        DefaultDependencyDescriptor dd = new DefaultDependencyDescriptor(desc,
-                ModuleRevisionId.newInstance(org, name, rev), false, false, true);
-        dd.addDependencyConfiguration("default", "*");
-        desc.addDependency(dd);
-        return desc;
-    }
-
-    // add run on another JVM?
-    private void execute(Path jarName, String mainClass, String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, MalformedURLException {
-        ClassLoader cl = new URLClassLoader(new URL[]{jarName.toUri().toURL()});
-        Class c = cl.loadClass(mainClass);
-        Thread.currentThread().setContextClassLoader(cl);
-        String[] ag = new String[]{};
-        Method m = c.getMethod("main", new Class[]{ag.getClass()});
-        m.invoke(null, new Object[]{ag});
-    }
-
-    public void run(String org, String name, String rev, String mainClass, String... args) throws IOException, ParseException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        ModuleDescriptor desc = makeDescriptor(org, name, rev);
-        ResolveReport result = ivy.resolve(desc, new ResolveOptions());
-        ModuleDescriptor md = result.getModuleDescriptor();
-        ivy.retrieve(md.getModuleRevisionId(),
-                "out/[artifact](-[classifier]).[ext]",
-                new RetrieveOptions().setConfs(new String[]{"default"}));
-        execute(Paths.get("out", name + ".jar"), mainClass, args);
+    public void run(String organization, String name, String rev, String mainClass, String... args) throws Exception {
+        List<String> a = new ArrayList<>(8 + args.length);
+        a.add("-settings");
+        a.add(setting.toFile().getPath());
+        a.add("-dependency");
+        a.add(organization);
+        a.add(name);
+        a.add(rev);
+        a.add("-main");
+        a.add(mainClass);
+        a.addAll(Arrays.asList(args));
+        org.apache.ivy.Main.main(a.toArray(new String[0]));
     }
 }
